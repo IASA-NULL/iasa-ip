5dd9ffef4ebd06ff396dd6367eb7d3a5bdb62d31
Seohyun Lee <04seohyun@gmail.com>
2019-08-18 16:48:13 +0900

    Ready for electron-builder

--- index.js
+++ index.js
@@ -55,6 +55,22 @@ function createAlertWindow() {
     });
 }
 
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+    app.quit()
+}
+app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (win) {
+        if (win.isMinimized()) win.restore()
+        win.focus()
+    }
+    else {
+        createMainWindow();
+    }
+});
+
 let orgName, orgNetStat, orgDnsStat;
 o=1;
 
