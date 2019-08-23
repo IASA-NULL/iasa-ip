31ad3c78e12dfafdef36ba6274bb77b49a820165
Seohyun Lee <04seohyun@gmail.com>
2019-08-23 17:45:49 +0900

    Add Tray Icon

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, Tray, dialog } = require('electron');
 const path = require('path');
 const url = require('url');
-const webControl=require('./webControl.js');
+const webControl = require('./webControl.js');
+let tray = null
 
 let win, awin;
 
-m=1;
-f=0;
+m = 1;
+f = 0;
 
+function openMainWindow() {
+    if (win != null) {
+        if (!win.isVisible()) {
+            win.show()
+        }
+    }
+    else {
+        createMainWindow();
+    }
+}
+
+function askStopService() {
+    const dialogOptions = { type: 'info', buttons: ['예', '아니요'], message: '서비스를 종료하면 IP가 자동으로 바뀌지 않습니다.\n정말로 종료할까요?', title: "IP" }
+    dialog.showMessageBox(dialogOptions, i => { if (i == 0) app.exit() })
+}
+
+function createTray() {
+    tray = new Tray('./res/IPLogo.ico')
+    const contextMenu = Menu.buildFromTemplate([
+        { label: '열기', click: openMainWindow },
+        { label: '서비스 종료', click: askStopService }
+    ])
+    tray.setToolTip('IP가 실행 중입니다.')
+    tray.setContextMenu(contextMenu)
+    tray.on('click', openMainWindow);
+}
 
 function createMainWindow() {
     win = new BrowserWindow({
         width: 850, height: 600, webPreferences: {
             nodeIntegration: true,
             webSecurity: false
-        }, show:false, icon: path.join(__dirname, 'res/ipLogo.ico')
+        }, show: false, icon: path.join(__dirname, 'res/ipLogo.ico')
     })
     win.setMenu(null);
     win.loadURL(url.format({
@@ -23,8 +50,8 @@ function createMainWindow() {
         slashes: true
     }));
     //win.webContents.openDevTools();
-    win.on('close', function(e){
-        win=null;
+    win.on('close', function (e) {
+        win = null;
     });
     win.once('ready-to-show', () => {
         win.show();
@@ -36,7 +63,7 @@ function createAlertWindow() {
         width: 600, height: 200, webPreferences: {
             nodeIntegration: true,
             webSecurity: false,
-        }, frame:false, transparent:true, show:false,icon: path.join(__dirname, 'res/ipLogo.ico')
+        }, frame: false, transparent: true, show: false, icon: path.join(__dirname, 'res/ipLogo.ico')
     })
     awin.setMenu(null);
     awin.loadURL(url.format({
@@ -48,12 +75,17 @@ function createAlertWindow() {
     //awin.webContents.openDevTools()
     awin.setAlwaysOnTop(true, "floating", 1);
     awin.setIgnoreMouseEvents(true);
-    awin.on('close', function(e){
-        awin=null;
+    awin.on('close', function (e) {
+        awin = null;
     });
     awin.once('ready-to-show', () => {
         awin.show();
     });
+}
+
+function onFirstRun() {
+    createMainWindow();
+    createTray();
 }
 
 const gotTheLock = app.requestSingleInstanceLock()
@@ -61,50 +93,38 @@ const gotTheLock = app.requestSingleInstanceLock()
 if (!gotTheLock) {
     app.quit()
 }
-app.on('second-instance', (event, commandLine, workingDirectory) => {
-    // Someone tried to run a second instance, we should focus our window.
-    if (win) {
-        if (win.isMinimized()) win.restore()
-        win.focus()
-    }
-    else {
-        createMainWindow();
-    }
-});
-
-
+app.on('second-instance', openMainWindow);
 
 
 let orgName, orgNetStat, orgDnsStat;
-o=1;
+o = 1;
 
-fir=true;
+fir = true;
 
-app.on('ready', createMainWindow);
+app.on('ready', onFirstRun);
 
-app.on('window-all-closed', function(e){
+app.on('window-all-closed', function (e) {
     e.preventDefault();
 });
 
-async function makeAlert()
-{
-    var tName=await webControl.getWifiName();
-    var tDnsStat=await webControl.checkDns();
-    var tNetStat=await webControl.checkInternet();
-    if(tName!=orgName || tNetStat!=orgNetStat || tDnsStat!=orgDnsStat) {
-        if(!fir && !tNetStat) {
-            if(win==null && awin==null) {
+async function makeAlert() {
+    var tName = await webControl.getWifiName();
+    var tDnsStat = await webControl.checkDns();
+    var tNetStat = await webControl.checkInternet();
+    if (tName != orgName || tNetStat != orgNetStat || tDnsStat != orgDnsStat) {
+        if (!fir && !tNetStat) {
+            if (win == null && awin == null) {
                 createAlertWindow();
             }
         }
-        fir=false;
+        fir = false;
     }
-    orgName=tName;
-    orgNetStat=tNetStat;
-    orgDnsStat=tDnsStat;
+    orgName = tName;
+    orgNetStat = tNetStat;
+    orgDnsStat = tDnsStat;
 }
 
-setInterval(function() {
+setInterval(function () {
     makeAlert();
     //localStorage.setItem('chgStat', 10);
 }, 1000);
