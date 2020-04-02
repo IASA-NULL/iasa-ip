67ebce16cbd50d043ba179fc3a6953366ac3d6af
Seohyun Lee <04seohyun@gmail.com>
2020-04-03 02:18:10 +0900

    Cleand Up Dependencies

--- webControl.js
+++ webControl.js
@@ -1,25 +1,4 @@
 const wifiName = require('wifi-name');
-const network = require('network');
-const dns = require('dns');
-
-function checkDns() {
-    return new Promise((resolve) => {
-        dns.lookupService('8.8.8.8', 53, err => {
-            if (err == null) resolve(true);
-            else resolve(false);
-        });
-    });
-}
-
-function checkInternet() {
-    return new Promise((resolve) => {
-        require("request")({url: 'http://www.msftconnecttest.com/', timeout: 1000}, (e, response) => {
-            if (e) resolve(false);
-            else if ('<pre>' === response.body.substr(0, 5)) resolve(true);
-            else resolve(false);
-        });
-    });
-}
 
 function getWifiName() {
     try {
@@ -29,16 +8,4 @@ function getWifiName() {
     }
 }
 
-function checkLanCable() {
-    return new Promise((resolve) => {
-        network.get_active_interface((err, obj) => {
-            if (String(obj.type) === 'Wired') resolve(true);
-            else resolve(false);
-        });
-    });
-}
-
-exports.checkInternet = checkInternet;
 exports.getWifiName = getWifiName;
-exports.checkDns = checkDns;
-exports.checkLanCable = checkLanCable;
