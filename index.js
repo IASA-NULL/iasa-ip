a052fe29931fd2f508c6ec058ef19a4c29a550c7
Seo_Hyun <04seohyun@gmail.com>
2020-07-03 08:56:28 +0900

    Added what's new

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ const gameList = ['Bluestacks.exe', 'League of legends.exe', 'riotclientservices
 
 
 function updateIP() {
-    win.close();
+    try {
+        win.close();
+    } catch (e) {
+
+    }
     request('https://api.iasa.kr/ip/link/lastest', (error, response, url) => {
         let notification = new Notification({
             title: '업데이트 중...',
